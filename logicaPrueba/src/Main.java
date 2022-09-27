c67aa697c64f4b52102ed3bf454a41b44f0c1edc
narvaezjacome <narvaezjacome@gmail.com>
2022-09-26 21:55:25 -0500

    se sube avance del reto del día de hoy

---
+++ logicaPrueba/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+       // Scanner sc = new Scanner(System.in);
+        //System.out.print("Agregue un valor de 1 - 9: ");
+
+//leé el digito del usuario
+       // int n = sc.nextInt();
+
+
+        int n = (5);
+
+//Cambia el valor acorde al numero
+        switch (n) {
+            case 0:
+                System.out.println(" _ \n| | \n|_|");
+                break;
+            case 1:
+                System.out.println("   \n  |\n  |");
+                break;
+            case 2:
+                System.out.println(" _ \n _|\n|_ ");
+                break;
+            case 3:
+                System.out.println(" _ \n _|\n _|");
+                break;
+            case 4:
+                System.out.println("   \n|_|\n  |");
+                break;
+            case 5:
+                System.out.println(" _ \n|_ \n _|");
+                break;
+            case 6:
+                System.out.println(" _ \n|_ \n|_|");
+                break;
+            case 7:
+                System.out.println(" _ \n  |\n  |");
+                break;
+            case 8:
+                System.out.println(" _ \n|_|\n|_|");
+                break;
+            case 9:
+                System.out.println(" _ \n|_|\n _|");
+                break;
+
+
+
+
+
+
+        }
+    }
+}
